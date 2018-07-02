10ceb152818c896a8bfd8d1781cf8264b5eab2fa
Mike Lovely <mike-lovely@hotmail.co.uk>
2018-07-02 18:36:02 +0100

    Remove stripping includes and relationships from response as this is now handled outside library.

--- lib/Handlers/AbstractHttpHandler.php
+++ lib/Handlers/AbstractHttpHandler.php
@@ -20,73 +20,6 @@ abstract class AbstractHttpHandler
         $this->httpClientWrapper = $httpClientWrapper;
     }
 
-    /**
-     * Merge any included data in JSON API spec into main object
-     *
-     * @param object $original The original JSON API response
-     */
-    protected function mergeIncludedData(&$original)
-    {
-        foreach ($original->data as &$resource) {
-            if (!property_exists($resource, 'relationships')) {
-                continue;
-            }
-            $this->iterateRelationshipsAndApplyIncludedData($resource, $original);
-        }
-    }
-
-    /**
-     * Iterate over relationships of a single to apply included data
-     *
-     * @param object $resource The parent resource to iterate over child relationhsis
-     * @param object $original The original JSON API response
-     */
-    private function iterateRelationshipsAndApplyIncludedData(&$resource, $original)
-    {
-        foreach ($resource->relationships as $type => &$data) {
-            if (is_object($data->data)) {
-                $this->addIncludeDataToModel($original->included, $data->data);
-            } elseif (is_array($data->data)) {
-                foreach ($data->data as &$nestedData) {
-                    $this->addIncludeDataToModel($original->included, $nestedData);
-                }
-            }
-        }
-    }
-
-    /**
-     * Find included data and add to resource model
-     *
-     * @param array $included The included resource models
-     * @param object $model The parent data model to add included data into
-     */
-    private function addIncludeDataToModel($included, &$model)
-    {
-        foreach ($included as $inclusion) {
-            if ($inclusion->type === $model->type
-                && $inclusion->id === $model->id)
-            {
-                $model->attributes = $inclusion->attributes;
-                break;
-            }
-        }
-    }
-
-    /**
-     * @param array $requested
-     * @param array $allowed
-     *
-     * @throws \Exception
-     */
-    protected function validateInclusions(array $requested, array $allowed)
-    {
-        foreach ($requested as $test) {
-            if (!in_array($test, $allowed)) {
-                throw new \Exception('Included property not allowed on this resource');
-            }
-        }
-    }
-
     protected function parseJsonApiResourceResponse(ResponseInterface $response)
     {
         $json = json_decode($response->getBody()->getContents());
@@ -95,12 +28,6 @@ abstract class AbstractHttpHandler
         if (!is_array($json->data)) {
             $json->data = [$json->data];
         }
-
-        // Merge any included data into parent resource(s)
-        if (property_exists($json, 'included')) {
-            $this->mergeIncludedData($json);
-        }
-
 
         // Rewind the response
         $response->getBody()->rewind();
