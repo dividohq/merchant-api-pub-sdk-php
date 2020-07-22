ee3db0c4a2f618057077860f614582970b9ccaeb
Steve Hill <steve.hill@divido.com>
2020-07-22 11:22:47 +0100

    ci: add PHPStan workflow (#15)
    * ci: add PHPStan workflow
    
    * Fixes for level 1 of PHPStan

---
+++ constants.php
@@ -0,0 +1,4 @@
+<?php
+defined('APP_PATH') || define('APP_PATH', 'lib');
+defined('DIVIDO_SOURCE_PATH') || define('DIVIDO_SOURCE_PATH', 'lib');
+defined('DIVIDO_APPLICATION_ENVIRONMENT') || define('DIVIDO_APPLICATION_ENVIRONMENT', 'functional');
