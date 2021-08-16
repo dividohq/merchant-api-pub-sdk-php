b13e5432fa8d56b09b319144b0c1a7181a891c39
Dillon Montag <dillon.montag@divido.com>
2021-08-16 16:43:13 +0100

    chore: update env test to use data provider

--- tests/unit/EnvironmentTest.php
+++ tests/unit/EnvironmentTest.php
@@ -9,42 +9,24 @@ class EnvironmentTest extends MerchantSDKTestCase
     /**
      * Test getting the correct environment based on the api key
      *
+     * @dataProvider provider_test_GettingEnvironment_WithSuppliedAPIKey_ReturnsCorrectEnvironment
      */
-    public function test_GettingEnvironment_WithSuppliedAPIKey_ReturnsCorrectEnvironment()
+    public function test_GettingEnvironment_WithSuppliedAPIKey_ReturnsCorrectEnvironment($suppliedArgument, $expectedResponse)
     {
-        // Dev api keys should be correctly identified
-        $apiKey = 'dev_f9425ece.3ec313437567c831d289efbdc240dd75';
-        $result = Environment::getEnvironmentFromAPIKey($apiKey);
-        $this->assertSame("dev", $result);
+        $result = Environment::getEnvironmentFromAPIKey($suppliedArgument);
+        $this->assertSame($expectedResponse, $result);
+    }
 
-        // Testing api keys should be correctly identified
-        $apiKey = 'testing_pk_f9425ece.3ec313437567c831d289efbdc240dd75';
-        $result = Environment::getEnvironmentFromAPIKey($apiKey);
-        $this->assertSame("testing", $result);
-
-        // Staging api keys should be correctly identified
-        $apiKey = 'staging_pk_f9425ece.3ec313437567c831d289efbdc240dd75';
-        $result = Environment::getEnvironmentFromAPIKey($apiKey);
-        $this->assertSame("staging", $result);
-
-        // Sandbox api keys should be correctly identified
-        $apiKey = 'sandbox_f9425ece.3ec313437567c831d289efbdc240dd75';
-        $result = Environment::getEnvironmentFromAPIKey($apiKey);
-        $this->assertSame("sandbox", $result);
-
-        // UAT api keys should be correctly identified
-        $apiKey = 'user-acceptance-testing_pk_f9425ece.3ec313437567c831d289efbdc240dd75';
-        $result = Environment::getEnvironmentFromAPIKey($apiKey);
-        $this->assertSame("user-acceptance-testing", $result);
-
-        // Production api keys should be correctly identified
-        $apiKey = 'production_f9425ece.3ec313437567c831d289efbdc240dd75';
-        $result = Environment::getEnvironmentFromAPIKey($apiKey);
-        $this->assertSame("production", $result);
-
-        // Live api keys should be correctly identified
-        $apiKey = 'live_pk_f9425ece.3ec313437567c831d289efbdc240dd75';
-        $result = Environment::getEnvironmentFromAPIKey($apiKey);
-        $this->assertSame("production", $result);
+    public function provider_test_GettingEnvironment_WithSuppliedAPIKey_ReturnsCorrectEnvironment()
+    {
+        return [
+            ["dev_f9425ece.3ec313437567c831d289efbdc240dd75'", "dev"],
+            ["testing_pk_f9425ece.3ec313437567c831d289efbdc240dd75", "testing"],
+            ["staging_pk_f9425ece.3ec313437567c831d289efbdc240dd75", "staging"],
+            ["sandbox_f9425ece.3ec313437567c831d289efbdc240dd75", "sandbox"],
+            ["user-acceptance-testing_pk_f9425ece.3ec313437567c831d289efbdc240dd75", "user-acceptance-testing"],
+            ["production_f9425ece.3ec313437567c831d289efbdc240dd75", "production"],
+            ["live_pk_f9425ece.3ec313437567c831d289efbdc240dd75", "production"]
+        ];
     }
 }
