3e297b5cc9421e1fd807a7afcd766112404391bc
David Thai <david.thai@divido.com>
2021-08-26 11:39:33 +0100

    chore: ci-fmt

--- constants.php
+++ constants.php
@@ -1,4 +1,5 @@
 <?php
+
 defined('APP_PATH') || define('APP_PATH', 'lib');
 defined('DIVIDO_SOURCE_PATH') || define('DIVIDO_SOURCE_PATH', 'lib');
 defined('DIVIDO_APPLICATION_ENVIRONMENT') || define('DIVIDO_APPLICATION_ENVIRONMENT', 'functional');
