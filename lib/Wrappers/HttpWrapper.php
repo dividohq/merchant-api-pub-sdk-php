b18179ba1c41408fb3d2d5f1a5d7af938e2bd566
Andrew Smith <42767996+andivido@users.noreply.github.com>
2023-12-07 15:08:57 +0000

    feat: handle new createRequest parameters

--- lib/Wrappers/HttpWrapper.php
+++ lib/Wrappers/HttpWrapper.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace Divido\MerchantSDK\Wrappers;
 
+use Divido\MerchantSDK\Exceptions\InvalidRequestException;
 use Divido\MerchantSDK\Exceptions\MerchantApiBadResponseException;
 use Http\Discovery\Psr17FactoryDiscovery;
 use Http\Discovery\Psr18ClientDiscovery;
-use Http\Message\RequestFactory;
 use Psr\Http\Client\ClientExceptionInterface;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamFactoryInterface;
 
 class HttpWrapper implements WrapperInterface
 {
@@ -33,26 +34,34 @@ class HttpWrapper implements WrapperInterface
     private $baseUrl;
 
     /**
-     * @var RequestFactory
+     * @var RequestFactoryInterface
      */
     private $requestFactory;
+
+    /**
+     * @var StreamFactoryInterface
+     */
+    private $streamFactory;
 
     /**
      * @param string $baseUrl
      * @param string $apiKey
      * @param ClientInterface $httpClient
-     * @param RequestFactory $requestFactory
+     * @param RequestFactoryInterface $requestFactory
+     * @param StreamFactoryInterface $streamFactory
      */
     public function __construct(
         string $baseUrl,
         string $apiKey,
         ClientInterface $httpClient = null,
-        RequestFactoryInterface $requestFactory = null
+        RequestFactoryInterface $requestFactory = null,
+        StreamFactoryInterface $streamFactory = null
     ) {
         $this->baseUrl = $baseUrl;
         $this->apiKey = $apiKey;
         $this->httpClient = $httpClient ?: Psr18ClientDiscovery::find();
         $this->requestFactory = $requestFactory ?: Psr17FactoryDiscovery::findRequestFactory();
+        $this->streamFactory = $streamFactory ?: Psr17FactoryDiscovery::findStreamFactory(); 
     }
 
     /**
@@ -75,10 +84,20 @@ class HttpWrapper implements WrapperInterface
 
         $request = $this->requestFactory->createRequest(
             strtoupper($method), 
-            $uri, 
-            $headers, 
-            $body
+            $uri
         );
+
+        if(is_string($body) == 'string') {
+            $request = $request->withBody($this->streamFactory->createStream($body));
+        } elseif (class_implements(\Psr\Http\Message\StreamInterface::class)){
+            $request = $request->withBody($body);
+        } elseif ($body !== null){
+            throw new InvalidRequestException("Unexpected request body received");
+        }
+        
+        foreach($headers as $key=>$value){
+            $request = $request->withHeader($key, $value);
+        }
 
         try {
             $response = $this->httpClient->sendRequest($request);
