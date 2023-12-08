ed896108783af9feaccb29d4bb7d1ba8f975050c
Andrew Smith <42767996+andivido@users.noreply.github.com>
2023-12-08 10:58:50 +0000

    chore: remove quickly obsoleted Exception

--- lib/Wrappers/HttpWrapper.php
+++ lib/Wrappers/HttpWrapper.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Divido\MerchantSDK\Wrappers;
 
-use Divido\MerchantSDK\Exceptions\InvalidRequestException;
 use Divido\MerchantSDK\Exceptions\MerchantApiBadResponseException;
 use Http\Discovery\Psr17FactoryDiscovery;
 use Http\Discovery\Psr18ClientDiscovery;
